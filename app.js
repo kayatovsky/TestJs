f8a3219153a56a837d3243782b035231cb952ff1
kayatovsky <Arxange1o@yandex.ru>
2021-06-28 19:23:04 +0300

    -

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 const express = require('express');
+
 const app = express();
 const port = 7000;
-const { spawn } = require('child_process');
+const {exec} = require('child_process');
 
 app.use(express.json());
 
 const quadraticEquation = (a, b, c) => {
-    if(a === 0)
-        return false;
+    if (a === 0) return false;
     let res;
-    let D = b * b - 4 * a * c;
-    if(D < 0)
-        return false;
-    if(D === 0)
-        res = (-b + Math.sqrt(D)) / (2 * a).toString();
-    else if(D > 0){
+    const D = b * b - 4 * a * c;
+    if (D < 0) return false;
+    if (D === 0) res = (-b + Math.sqrt(D)) / (2 * a).toString();
+    else if (D > 0) {
         res = (-b + Math.sqrt(D)) / (2 * a).toFixed(2).toString();
-        res = res + ", " + (-b - Math.sqrt(D)) / (2 * a).toFixed(2).toString()
+        res = `${res}, ${(-b - Math.sqrt(D)) / (2 * a).toFixed(2).toString()}`;
     }
     return res;
 };
 
 app.post('/quadratic-equation', (req, res) => {
-    const expression = req.body.expression;
-    let coefficients = expression
+    const {expression} = req.body;
+    const coefficients = expression
         .replace('**2', '')
         .replace(/ /g, '') // удалить пробелы
         .replace(/(?<!\d)x/g, '1x') // добавить 1 к x, у которого нет номера
@@ -34,46 +32,62 @@ app.post('/quadratic-equation', (req, res) => {
     if (coefficients.length !== 3) {
         res.status(400).send(
             {
-                error: "invalid expression"
-            }
-        )
+                error: 'invalid expression',
+            },
+        );
     } else {
-        let roots = quadraticEquation(
+        const roots = quadraticEquation(
             Number(coefficients[0]),
             Number(coefficients[1]),
-            Number(coefficients[2])
+            Number(coefficients[2]),
         );
 
-        console.log("Request: ", expression);
-        console.log("Coefficients: ", coefficients);
-        console.log("Roots: ", roots);
+        console.log('Request: ', expression);
+        console.log('Coefficients: ', coefficients);
+        console.log('Roots: ', roots);
 
         if (roots === false) {
             res.status(400).send(
                 {
-                    error: 'quadratic equation has no solution'
-                }
-            )
+                    error: 'quadratic equation has no solution',
+                },
+            );
         }
 
         res.status(200).send(
             {
-                result: roots
-            }
-        )
+                result: roots,
+            },
+        );
     }
 });
 
 app.get('/cpu-info', (req, res) => {
-    const promise = new Promise((resolve, reject) => {
-        resolve('foo');
+    const promise = new Promise(async (resolve, reject) => {
+        const {stdout} = await exec('cat /proc/cpuinfo', (error, stdout, stderr) => {
+            if(error) {
+                console.error("Exec error: ", error)
+                return false;
+            }
+            // console.log("stdout: ", stdout)
+            // console.error("stderr: ", stderr)
+            return stdout
+        });
+        resolve(stdout);
     });
     promise.then((value) => {
         console.log(value);
+        if(value === false) {
+            res.status(500).send(
+                {
+                    error: "server error"
+                }
+            )
+        } else {
+            res.status(200).send(value)
+        }
     });
-    console.log(promise);
-
+    // console.log(promise);
 });
-
 
 app.listen(port, () => console.log(`Server listens ${port}`));
