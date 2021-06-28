50fd3d25a62036f31c132a91e6ac2e27bac874ef
kayatovsky <Arxange1o@yandex.ru>
2021-06-28 20:24:41 +0300

    -

--- app.js
+++ app.js
@@ -2,93 +2,89 @@ const express = require('express');
 
 const app = express();
 const port = 7000;
-const {exec} = require('child_process');
+const { exec } = require('child_process');
 
 app.use(express.json());
 
 const quadraticEquation = (a, b, c) => {
-    if (a === 0) return false;
-    let res;
-    const D = b * b - 4 * a * c;
-    if (D < 0) return false;
-    if (D === 0) res = Math.round((-b + Math.sqrt(D)) / (2 * a)).toString();
-    else if (D > 0) {
-        res = Math.round((-b + Math.sqrt(D)) / (2 * a)).toString();
-        res = `${res}, ${Math.round((-b - Math.sqrt(D)) / (2 * a)).toString()}`;
-    }
-    return res;
+  if (a === 0) return false;
+  let res;
+  const D = b * b - 4 * a * c;
+  if (D < 0) return false;
+  if (D === 0) res = Math.round((-b + Math.sqrt(D)) / (2 * a)).toString();
+  else if (D > 0) {
+    res = Math.round((-b + Math.sqrt(D)) / (2 * a)).toString();
+    res = `${res}, ${Math.round((-b - Math.sqrt(D)) / (2 * a)).toString()}`;
+  }
+  return res;
 };
 
 app.post('/quadratic-equation', (req, res) => {
-    const {expression} = req.body;
-    const coefficients = expression
-        .replace('**2', '')
-        .replace(/ /g, '') // удалить пробелы
-        .replace(/(?<!\d)x/g, '1x') // добавить 1 к x, у которого нет номера
-        .replace(/=.*/, '') // убрать равную и правую часть уравнения
-        .replace('+', '')
-        .split(/x/);
+  const { expression } = req.body;
+  const coefficients = expression
+    .replace('**2', '')
+    .replace(/ /g, '') // удалить пробелы
+    .replace(/(?<!\d)x/g, '1x') // добавить 1 к x, у которого нет номера
+    .replace(/=.*/, '') // убрать равную и правую часть уравнения
+    .replace('+', '')
+    .split(/x/);
 
-    if (coefficients.length !== 3) {
-        res.status(400).send(
-            {
-                error: 'invalid expression',
-            },
-        );
-    } else {
-        const roots = quadraticEquation(
-            Number(coefficients[0]),
-            Number(coefficients[1]),
-            Number(coefficients[2]),
-        );
+  if (coefficients.length !== 3) {
+    res.status(400).send(
+      {
+        error: 'invalid expression',
+      },
+    );
+  } else {
+    const roots = quadraticEquation(
+      Number(coefficients[0]),
+      Number(coefficients[1]),
+      Number(coefficients[2]),
+    );
 
-        console.log('Request: ', expression);
-        console.log('Coefficients: ', coefficients);
-        console.log('Roots: ', roots);
+    console.log('Request: ', expression);
+    console.log('Coefficients: ', coefficients);
+    console.log('Roots: ', roots);
 
-        if (roots === false) {
-            res.status(400).send(
-                {
-                    error: 'quadratic equation has no solution',
-                },
-            );
-        }
-
-        res.status(200).send(
-            {
-                result: roots,
-            },
-        );
+    if (roots === false) {
+      res.status(400).send(
+        {
+          error: 'quadratic equation has no solution',
+        },
+      );
     }
+
+    res.status(200).send(
+      {
+        result: roots,
+      },
+    );
+  }
 });
 
 app.get('/cpu-info', (req, res) => {
-    const promise = new Promise(async (resolve, reject) => {
-        const {stdout} = await exec('cat /proc/cpuinfo', (error, stdout, stderr) => {
-            if(error) {
-                console.error("Exec error: ", error)
-                return false;
-            }
-            // console.log("stdout: ", stdout)
-            // console.error("stderr: ", stderr)
-            return stdout
-        });
-        resolve(stdout);
+  const promise = new Promise((resolve) => {
+    const { stdout } = exec('cat /proc/cpuinfo', (error, stdout) => {
+      if (error) {
+        console.error('Exec error: ', error);
+        return false;
+      }
+      return stdout;
     });
-    promise.then((value) => {
-        console.log(value);
-        if(value === false) {
-            res.status(500).send(
-                {
-                    error: "server error"
-                }
-            )
-        } else {
-            res.status(200).send(value)
-        }
-    });
-    // console.log(promise);
+    resolve(stdout);
+  });
+  promise.then((value) => {
+    console.log(value);
+    if (value === false) {
+      res.status(500).send(
+        {
+          error: 'server error',
+        },
+      );
+    } else {
+      res.status(200).send(value);
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Server listens ${port}`));
-
--- test.js
+++ test.js
@@ -1,126 +1,127 @@
+/* globals assert, describe, it */
 const axios = require('axios');
 
 describe('test-quadratic-equation', () => {
-    it('2x**2+5x-10=0', () => {
-        axios.post('http://localhost:7000/quadratic-equation',
-            {
-                expression: '2x**2+5x-10=0',
-            }).then((response) => {
-            assert.equal(response, {
-                "result": "1, -4"
-            });
-        });
+  it('2x**2+5x-10=0', () => {
+    axios.post('http://localhost:7000/quadratic-equation',
+      {
+        expression: '2x**2+5x-10=0',
+      }).then((response) => {
+      assert.equal(response, {
+        result: '1, -4',
+      });
     });
+  });
 
-    it('-2x**2+5x-10=0', () => {
-        axios.post('http://localhost:7000/quadratic-equation',
-            {
-                expression: '-2x**2+5x-10=0',
-            }).then((response) => {
-            assert.equal(response, {
-                "error": 'quadratic equation has no solution'
-            });
-        });
+  it('-2x**2+5x-10=0', () => {
+    axios.post('http://localhost:7000/quadratic-equation',
+      {
+        expression: '-2x**2+5x-10=0',
+      }).then((response) => {
+      assert.equal(response, {
+        error: 'quadratic equation has no solution',
+      });
     });
+  });
 
-    it('-10=0', () => {
-        axios.post('http://localhost:7000/quadratic-equation',
-            {
-                expression: '-10=0',
-            }).then((response) => {
-            assert.equal(response, {
-                "error": 'invalid expression'
-            });
-        });
+  it('-10=0', () => {
+    axios.post('http://localhost:7000/quadratic-equation',
+      {
+        expression: '-10=0',
+      }).then((response) => {
+      assert.equal(response, {
+        error: 'invalid expression',
+      });
     });
+  });
 });
 
 describe('test-cpu-info', () => {
-    it('get-cpu-info', () => {
-        axios.get('http://localhost:7000/cpu-info')
-            .then((response) => {
-            assert.equal(response, {
-              "connecting": false,
-              "_hadError": false,
-              "_handle": {
-                "reading": true
-              },
-              "_parent": null,
-              "_host": null,
-              "_readableState": {
-                "objectMode": false,
-                "highWaterMark": 16384,
-                "buffer": {
-                  "head": null,
-                  "tail": null,
-                  "length": 0
-                },
-                "length": 0,
-                "pipes": null,
-                "pipesCount": 0,
-                "flowing": true,
-                "ended": false,
-                "endEmitted": false,
-                "reading": true,
-                "sync": false,
-                "needReadable": true,
-                "emittedReadable": false,
-                "readableListening": false,
-                "resumeScheduled": false,
-                "paused": false,
-                "emitClose": false,
-                "autoDestroy": false,
-                "destroyed": false,
-                "defaultEncoding": "utf8",
-                "awaitDrain": 0,
-                "readingMore": false,
-                "decoder": {
-                  "encoding": "utf8"
-                },
-                "encoding": "utf8"
-              },
-              "readable": true,
-              "_events": {},
-              "_eventsCount": 3,
-              "_writableState": {
-                "objectMode": false,
-                "highWaterMark": 16384,
-                "finalCalled": false,
-                "needDrain": false,
-                "ending": false,
-                "ended": false,
-                "finished": false,
-                "destroyed": false,
-                "decodeStrings": false,
-                "defaultEncoding": "utf8",
-                "length": 0,
-                "writing": false,
-                "corked": 0,
-                "sync": true,
-                "bufferProcessing": false,
-                "writecb": null,
-                "writelen": 0,
-                "bufferedRequest": null,
-                "lastBufferedRequest": null,
-                "pendingcb": 0,
-                "prefinished": false,
-                "errorEmitted": false,
-                "emitClose": false,
-                "autoDestroy": false,
-                "bufferedRequestCount": 0,
-                "corkedRequestsFree": {
-                  "next": null,
-                  "entry": null
-                }
-              },
-              "writable": false,
-              "allowHalfOpen": false,
-              "_sockname": null,
-              "_pendingData": null,
-              "_pendingEncoding": "",
-              "server": null,
-              "_server": null
-            });
+  it('get-cpu-info', () => {
+    axios.get('http://localhost:7000/cpu-info')
+      .then((response) => {
+        assert.equal(response, {
+          connecting: false,
+          _hadError: false,
+          _handle: {
+            reading: true,
+          },
+          _parent: null,
+          _host: null,
+          _readableState: {
+            objectMode: false,
+            highWaterMark: 16384,
+            buffer: {
+              head: null,
+              tail: null,
+              length: 0,
+            },
+            length: 0,
+            pipes: null,
+            pipesCount: 0,
+            flowing: true,
+            ended: false,
+            endEmitted: false,
+            reading: true,
+            sync: false,
+            needReadable: true,
+            emittedReadable: false,
+            readableListening: false,
+            resumeScheduled: false,
+            paused: false,
+            emitClose: false,
+            autoDestroy: false,
+            destroyed: false,
+            defaultEncoding: 'utf8',
+            awaitDrain: 0,
+            readingMore: false,
+            decoder: {
+              encoding: 'utf8',
+            },
+            encoding: 'utf8',
+          },
+          readable: true,
+          _events: {},
+          _eventsCount: 3,
+          _writableState: {
+            objectMode: false,
+            highWaterMark: 16384,
+            finalCalled: false,
+            needDrain: false,
+            ending: false,
+            ended: false,
+            finished: false,
+            destroyed: false,
+            decodeStrings: false,
+            defaultEncoding: 'utf8',
+            length: 0,
+            writing: false,
+            corked: 0,
+            sync: true,
+            bufferProcessing: false,
+            writecb: null,
+            writelen: 0,
+            bufferedRequest: null,
+            lastBufferedRequest: null,
+            pendingcb: 0,
+            prefinished: false,
+            errorEmitted: false,
+            emitClose: false,
+            autoDestroy: false,
+            bufferedRequestCount: 0,
+            corkedRequestsFree: {
+              next: null,
+              entry: null,
+            },
+          },
+          writable: false,
+          allowHalfOpen: false,
+          _sockname: null,
+          _pendingData: null,
+          _pendingEncoding: '',
+          server: null,
+          _server: null,
         });
-    });
+      });
+  });
 });
